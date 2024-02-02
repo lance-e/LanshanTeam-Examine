77998cab28d462ab907d834b4578787b16ec82aa
lance547 <437653103@qq.com>
2024-02-03 01:17:26 +0800

    fix:add etcd as the center of service discovery and register

--- go.mod
+++ go.mod
@@ -32,6 +32,8 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
 	github.com/clbanning/mxj/v2 v2.5.5 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.3.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
@@ -41,6 +43,7 @@ require (
 	github.com/go-playground/validator/v10 v10.14.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
@@ -64,6 +67,9 @@ require (
 	github.com/tjfoc/gmsm v1.3.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.2.11 // indirect
+	go.etcd.io/etcd/api/v3 v3.5.12 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.5.12 // indirect
+	go.etcd.io/etcd/client/v3 v3.5.12 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	golang.org/x/arch v0.3.0 // indirect
 	golang.org/x/crypto v0.18.0 // indirect
@@ -71,6 +77,8 @@ require (
 	golang.org/x/net v0.20.0 // indirect
 	golang.org/x/sys v0.16.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/genproto v0.0.0-20231106174013-bbf56f31fb17 // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20231106174013-bbf56f31fb17 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231120223509-83a465c0220f // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
---
+++ server/game/serveRegister/register.go
@@ -0,0 +1,77 @@
+package serveRegister
+
+import (
+	"LanshanTeam-Examine/server/game/utils"
+	"context"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"time"
+)
+
+type EtcdRegister struct {
+	client  *clientv3.Client
+	leaseId clientv3.LeaseID
+}
+
+func NewEtcdRegister(addr string) (*EtcdRegister, error) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{addr},
+		DialTimeout: 3 * time.Second,
+	})
+	if err != nil {
+		utils.GameLogger.Error("newEtcdRegister failed,error:" + err.Error())
+		return nil, err
+	}
+	e := &EtcdRegister{
+		client: cli,
+	}
+	return e, nil
+}
+func (e *EtcdRegister) ServiceRegister(name string, add string, expire int64) error {
+	err := e.CreateLease(expire)
+	if err != nil {
+		return err
+	}
+	err = e.BindLease(name, add)
+	if err != nil {
+		return err
+	}
+	err = e.KeepAlive()
+	return err
+}
+func (e *EtcdRegister) CreateLease(expire int64) error {
+	lease, err := e.client.Grant(context.Background(), expire)
+	if err != nil {
+		utils.GameLogger.Debug("create lease failed")
+		return err
+	}
+	e.leaseId = lease.ID
+	return nil
+}
+func (e *EtcdRegister) BindLease(key string, value string) error {
+	_, err := e.client.Put(context.Background(), key, value, clientv3.WithLease(e.leaseId))
+	if err != nil {
+		utils.GameLogger.Debug("bind lease failed")
+		return err
+	}
+	return nil
+}
+func (e *EtcdRegister) KeepAlive() error {
+	resp, err := e.client.KeepAlive(context.Background(), e.leaseId)
+	if err != nil {
+		utils.GameLogger.Debug("keep alive failed")
+		return err
+	}
+	go func(resp <-chan *clientv3.LeaseKeepAliveResponse) {
+		for {
+			select {
+			case <-resp:
+				utils.GameLogger.Info("keep alive success")
+			}
+		}
+	}(resp)
+	return nil
+}
+func (e *EtcdRegister) Close() error {
+	e.client.Revoke(context.Background(), e.leaseId)
+	return e.client.Close()
+}
